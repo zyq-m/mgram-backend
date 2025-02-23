46596e464857b580ac74acb05420d9f6167d542c
haziq <haziq.musa02@gmail.com>
2025-02-24 05:01:41 +0800

    update seed

--- create_db.py
+++ create_db.py
@@ -5,9 +5,9 @@ from model import User
 def seed_db():
     db.create_all()
     admin = User(
-        email="admin@mgram.com", name="Fulan", phone_no="0123456789", role="ADMIN"
+        email="admin@mgram.net", name="Super Admin", phone_no="0123456789", role="ADMIN"
     )
-    user = User(email="ahmad@mgram.com", name="Ahmad", phone_no="0123456788")
+    user = User(email="ahmad@mgram.net", name="Ahmad", phone_no="0123456788")
 
     db.session.add(admin)
     db.session.add(user)
